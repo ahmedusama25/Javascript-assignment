69fabf262f1c0ca45c55542716915d622e97a6f8
ahmedusama25 <57043086+ahmedusama25@users.noreply.github.com>
2020-06-07 16:38:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,134 @@
+                                                // Assignment 1
+                                                
+                                                //Question No 1
+alert("Hellow world")
+
+                                                //Question No 2
+alert("Error! Please enter a valid password")
+
+                                                //Question No 3
+alert("welcome to JS Land \n Happy Coding!")
+
+                                                //Question No 4
+alert("Welcome to JS Land")
+alert("Happy Coding")
+                                                //Question No 5
+alert("Hello..I can run JS through my web browser console")
+                                                //Question No 6 & 7
+//done  
+
+                                                //Assignment 2
+
+                                                //Question No 1
+var username;
+                                                //Question No 2
+var myName="Ahmed Usama";
+                                                //Question No 3
+var message="Hellow World";
+alert(message)
+                                                //Question No 4:
+var Name = "Jhone Doe";
+var  Age = "My Age is 15" ;
+var Qualification="Certified Mobile Application Developer";
+alert(Name)
+alert(Age)
+alert(Qualification)
+                                                //Question No 5
+var a="PIZZA";
+alert(a + "\n" + a.slice(0,5) +  "\n" + a.slice(0,4) + "\n" + a.slice(0,3) + "\n" + a.slice(0,2) + "\n" + a.slice(0,1))
+                                                 //Question No 6
+var email="ahmed.usama25@hotmail.com";
+alert("my eamil address is: " + email)             
+                                                //Question No 7
+var book = "A smarter way to learn JavaScript";
+alert("i am trying to learn from the book " + book)
+                                                //Question No 8
+console.log("Yah1 i can write HTML content through Javascript")
+
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+document.write("▬▬▬▬▬▬▬▬▬END▬▬▬▬▬▬▬▬▬")
+                                                
+                                                //Assignment No 3
+                                                
+                                                //Question No 1
+var Myage="22";
+alert("My age is: " + Myage)
+                                                //Question No 2
+var a=0
+var b=a++
+alert("You have visited this site" + b + "times")
+                                                //Question No 3
+var Birthyear=1997;
+document.write("My birth year is" + Birthyear + "/nData type of my declared variable is number")
+                                                //Question No 4
+var Visitors_name="John Doe";
+var Product_title="5 T-shirt(s)";
+var Quantity="XYZ Clothing store";
+document.write(Visitors_name + "ordered" + Product_title + "from" + Quantity)
+
+                                                //Assignment 4
+
+                                                //Question No 1
+var a,b,c;
+                                                //Question No 2
+var a,b,c,d,e, f, g, h, i, j;
+                                                //Question No 3
+document.write("A heading stating Rules for naming JS variables \nVariable names can only contain ______, ______, ______ and ______. For example $my_1stVariable \n Variables must begin with a ______, ______ or _____. For example $name, _name or name \n Variable names are case _________ \n Variable names should not")
+
+                                                //assignment No. 5
+
+                                                //Question No 1
+var a=3;
+var b=5;
+var c= a + b;
+document.write("sum of" + a + "& " + b + "is " + c)
+                                                //Question No 2
+var d=3;
+var e=5;
+var f= e - d;
+var g= d * e;
+var h= d/e;
+var i= d%e;
+document.write("diff of" + d + "& " + e + "is " + f)
+document.write("product of" + d + "& " + e + "is " + g)
+document.write("division of" + d + "& " + e + "is " + h)
+document.write("mod of" + d + "& " + e + "is " + i)
+                                                //Question No 3
+var vari;
+document.write("value of the variable is:" + vari)
+vari = 5;
+document.write("initial value is:" + vari)
+vari= ++vari;
+document.write("value after increment is" + vari)
+vari= vari + 7;
+document.write("value after adding 7 is: " + vari)
+vari=--vari;
+document.write("value after decrement is: " + vari)
+var varia= vari % 3;
+document.write("the remainder is: " + varia)
+                                                //Question No 4
+var ticket = 600;
+var per= 5;
+var total= ticket * per;
+document.write("the price of " + no + "ticket is" + total)
+                                                //Question No 5
+var number= 4;
+for (var i=1; i<=10; i++){
+    mul=number * i;
+    document.write(number + "*" + i + "=" + mul + '\n')
+}
+                                                //Question No 6
+var C = 7
+var F = (C + 9/5) + 32
+document.write(F)
+                                                //Question No 
+                                                //Question No 
+                                                //Question No 
+                                                //Question No 
+                                                //Question No 
+                                                //Question No 
+                                                //Question No 
+
+                                                Assignment No: 6
+
+                                                //Question No 1
